7773466d801859fa1b93871dfca038b4aef62fe7
immortalking12 <102854204+immortalking12@users.noreply.github.com>
2022-06-28 22:26:17 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -13,7 +13,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"href='tg://user?id={@ImortalKingTG}'>ℐℳℳᎾℛᏆᎯℒ ᏦℐℕᎶ</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ source : <a href='https://heroku.com'>Click here</a>\n○ Channel : @TheSerialZone\n○ Support : @ImortalKingTG</b>",
+            text=f"<b>○ Creator : <a href='tg://user?id={@ImortalKingTG}'>ℐℳℳᎾℛᏆᎯℒ ᏦℐℕᎶ</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ source : <a href='https://heroku.com'>Click here</a>\n○ Channel : @TheSerialZone\n○ Support : @ImortalKingTG</b>",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("• Close💥 •", callback_data="close")]]
